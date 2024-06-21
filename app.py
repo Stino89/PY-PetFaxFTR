71ddf098b3deffc8cc4d064ba0cc1bc691128ab4
Faustino Ruiz <ftr1989@gmail.com>
2024-06-20 19:30:11 -0700

    zipzip

---
+++ app.py
@@ -0,0 +1,15 @@
+# Config
+from flask import Flask
+app = Flask(__name__)
+
+# Routes
+
+# Index Route
+@app.route('/')
+def index():
+	return "Hello, this is PetFax!"
+
+# Pets Index Route
+@app.route('/pets')
+def pets():
+	return 'These are our pets available for adoption!'
