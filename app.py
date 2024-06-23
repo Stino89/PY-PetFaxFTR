20189a6a6ce4b61a8ba50d62db85629fd3319f82
Faustino Ruiz <ftr1989@gmail.com>
2024-06-23 08:56:10 -0700

    homework

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-# Config
-from flask import Flask
-app = Flask(__name__)
+from petfax import create_app
 
-# Routes
-
-# Index Route
-@app.route('/')
-def index():
-	return "Hello, this is PetFax!"
-
-# Pets Index Route
-@app.route('/pets')
-def pets():
-	return 'These are our pets available for adoption!'
+app = create_app()
