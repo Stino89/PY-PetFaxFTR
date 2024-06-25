da9a30ccdea6cf6534e7d119845e2a82c92a236e
Faustino Ruiz <ftr1989@gmail.com>
2024-06-24 17:29:19 -0700

    missz

--- petfax/_init_.py
+++ petfax/_init_.py
@@ -1,9 +1,19 @@
-# config                    
+# config
 from flask import Flask
+import os
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    from . import models
+    models.db.init_app(app)
+
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
